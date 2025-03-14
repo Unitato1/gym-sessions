25dbba579f02378b2bb6d4b1cffa05bdd64b5b0d
Robert Svihla <xsvihla@fi.muni.cz>
2025-03-14 18:01:59 +0100

    add workout index

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -3,6 +3,9 @@ class WorkoutsController < ApplicationController
       @workout = Workout.find(params[:id])
       @workout_exercises = @workout.workout_exercises
   end
+  def index
+    @workouts = Workout.all
+  end
   def update
   end
 end
