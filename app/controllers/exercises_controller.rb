a2f511fb022aa5427dc189c0d5f6324e84a4547f
Robert Svihla <xsvihla@fi.muni.cz>
2025-03-17 22:44:29 +0100

    add crud for exercise

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -1,5 +1,42 @@
 class ExercisesController < ApplicationController
+  before_action :get_exercise, only: [ :edit, :destroy, :update, :show ]
   def index
     @exercises = Exercise.all
+  end
+  def show
+  end
+  def new
+    @exercise = Exercise.new
+  end
+  def create
+    if @exercise.save
+      redirect_to @exercise
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+  def update
+    if @exercise.update(exercise_parms)
+      redirect_to @exercise
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+  def edit
+  end
+
+  def destroy
+    if @exercise.destroy
+      redirect_to exercises_path
+    else
+      render @exercise
+    end
+  end
+  private
+  def exercise_parms
+    params.require(:exercise).permit(:name)
+  end
+  def get_exercise
+    @exercise = Exercise.find(params[:id])
   end
 end
