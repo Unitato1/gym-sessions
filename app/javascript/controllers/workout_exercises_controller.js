4b75aadb6c6f71895aca3a3acc29f3fd2a4efa5c
Robert Svihla <xsvihla@fi.muni.cz>
2025-03-18 15:22:11 +0100

    add feature to add more exercises on create and edit

---
+++ app/javascript/controllers/workout_exercises_controller.js
@@ -0,0 +1,45 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="workout-exercises"
+export default class extends Controller {
+  static targets = [ "template", "test" ]
+
+  initialize() {
+    this.index = Math.round(this.testTarget.childNodes.length / 6);
+  }
+
+  connect() {
+    console.log("Stimulus controller connected!");
+  }
+
+  addExercise() {
+    this.index++;
+
+    const exercise_form = this.templateTarget.content.cloneNode(true);
+
+    const nodes = exercise_form.querySelectorAll('*');
+    
+    nodes.forEach(node => {
+      this.updateNodeAttributes(node);
+    });
+
+    this.testTarget.appendChild(exercise_form);
+  }
+
+  updateNodeAttributes(node) {
+    if (node.name) {
+      const newName = node.name.replace(/\[\d+\]/, `[${this.index}]`);
+      node.setAttribute('name', newName);
+    }
+
+    if (node.id) {
+      const newId = node.id.replace(/\_\d+/, `_${this.index}`);
+      node.setAttribute('id', newId);
+    }
+
+    if (node.tagName.toLowerCase() === 'label' && node.getAttribute('for')) {
+      const newFor = node.getAttribute('for').replace(/\_\d+/, `_${this.index}`);
+      node.setAttribute('for', newFor);
+    }
+  }
+}
