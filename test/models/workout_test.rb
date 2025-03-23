a254d567575dfd5d269433646376277de988d0a5
Robert Svihla <xsvihla@fi.muni.cz>
2025-03-23 23:23:43 +0100

    add very basic tests

---
+++ test/models/workout_test.rb
@@ -0,0 +1,14 @@
+require "test_helper"
+
+class WorkoutTest < ActiveSupport::TestCase
+  test "should not save workout without a name" do
+    workout = Workout.new
+    assert_not workout.save, "Saved the workout without a name"
+  end
+
+  test "should belong to a user" do
+    user = users(:one) # fixtures in test/fixtures/users.yml
+    workout = user.workouts.build(name: "Leg Day")
+    assert workout.save, "Failed to save a valid workout"
+  end
+end
