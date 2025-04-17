d68fa0156864e0b24837e04cff7b895edddbc02b
Robert Svihla <xsvihla@fi.muni.cz>
2025-04-17 22:17:37 +0200

    add users show page for every user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,13 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
   def show
-    @workouts = current_user.workouts
-    @liked_workouts = current_user.likes
+    @user = User.find_by(id: params[:id])
+    if @user
+      @workouts = @user.workouts
+      @liked_workouts = @user.likes
+    else
+      flash[:alert] = "User not found"
+      redirect_to root_path
+    end
   end
 end
